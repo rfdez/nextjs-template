b77d12e8326d3322d51a293cfac891f366003615
Raúl Fernández Fernández <49290264+rfdez@users.noreply.github.com>
2025-05-12 16:28:06 +0200

    feat: prepare project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,18 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	eslintConfigPrettier,
 ];
 
 export default eslintConfig;
